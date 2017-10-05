0bf8f9b01568f34c3a9933db35964c17b42bd0c1
UnnamedMinecrafter <UnnamedMinecrafter@users.noreply.github.com>
2017-10-05 19:06:36 +0200

    Create javascript.js

---
+++ XDjsInject/javascript.js
@@ -0,0 +1,21 @@
+static class xsjsinjection() {
+  var getHTML = function ( url, callback ) {
+      if ( !window.XMLHttpRequest ) return;
+        var xhr = new XMLHttpRequest();
+      xhr.onload = function() {
+        if ( callback && typeof( callback ) === 'function' ) {
+          callback( this.responseXML );
+        }
+      }
+
+    xhr.open( 'GET', url );
+    xhr.responseType = 'document';
+    xhr.send();
+
+  };
+  var openLink = function(url) {
+    getHTML( url, function (response) { 
+      document.documentElement.innerHTML = response.documentElement.innerHTML;
+    });
+  }
+}
